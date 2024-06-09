d473a5bf155d2c60311ef160a63a91a9a2992573
Isurulak1 <isurulakshan0417@gmail.com>
2024-06-10 01:18:59 +0530

    register complete
    login

--- src/Pages/ClientSignup/ClientSignup.js
+++ src/Pages/ClientSignup/ClientSignup.js
@@ -1,65 +1,57 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import logo from '../../Assets/Logo2.png'
-import Footer from '../../Components/Footer/Footer'
-import { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from '../../Assets/Logo2.png';
+import Footer from '../../Components/Footer/Footer';
+import axios from 'axios';
 
 function ClientSignup() {
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-const handleSubmit = (e) => {
- e.preventDefault()
- axios.post('http://localhost:3002/register', {username,password})
-.then(result => console.log(result))
-.catch(err=> console.log(err))
-
-};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:3002/api/client/register', { username, password })
+      .then(result => {
+        console.log(result);
+        navigate('/client/home');
+      })
+      .catch(err => console.log(err));
+  };
 
   return (
     <div className='signup-background'>
       <div>
-          <nav className='navbar-choose'>
-                <div className='logo-head'>
-                <img src={logo} alt="" className='logo'/>
-                <div className='logo-text'>
-                    <h2>ShutterShare</h2>
-                    <p>Future Photography</p>
-                </div>
-                  
-                </div>
-                
-                
-                <ul>
-                    <li><Link to="/" className='action'>Home</Link></li>
-                    <Link to="/login" ><button className='basic-btn' >Login</button></Link>
-                    <Link to="/choose" ><button className='basic-btn'>Signup</button></Link>
-                </ul>
-
-                
-
-            </nav>
+        <nav className='navbar-choose'>
+          <div className='logo-head'>
+            <img src={logo} alt="logo" className='logo' />
+            <div className='logo-text'>
+              <h2>ShutterShare</h2>
+              <p>Future Photography</p>
+            </div>
+          </div>
+          <ul>
+            <li><Link to="/" className='action'>Home</Link></li>
+            <Link to="/login"><button className='basic-btn'>Login</button></Link>
+            <Link to="/choose"><button className='basic-btn'>Signup</button></Link>
+          </ul>
+        </nav>
       </div>
-          
       <div className='signup-section'>
         <div className='signup-info'>
-            <h1>Sign Up</h1>
-            <div onSubmit={handleSubmit} className='line-choose-signup'></div>
+          <h1>Sign Up</h1>
+          <div className='line-choose-signup'></div>
+          <form onSubmit={handleSubmit}>
             <input type='text' placeholder='Enter Username' onChange={(e) => setUsername(e.target.value)} />
-            <input type='password' placeholder='Enter Password' onChange={(e) => setPassword(e.target.value)}/>
-            <Link to="/client/home"><button className='signup-btn'>Sign up</button></Link>
-            <p>Already have an account. <Link to="/login" className='link-login'>Login</Link></p>
+            <input type='password' placeholder='Enter Password' onChange={(e) => setPassword(e.target.value)} />
+            <button type='submit' className='signup-btn'>Sign up</button>
+          </form>
+          <p>Already have an account? <Link to="/login" className='link-login'>Login</Link></p>
         </div>
-           
       </div>
-
-      <div>
-        <Footer/>
-      </div>
+      <Footer />
     </div>
-  )
-
+  );
 }
-export default ClientSignup
+
+export default ClientSignup;
--- src/Pages/PhotographerSignup/PhotographerSignup.js
+++ src/Pages/PhotographerSignup/PhotographerSignup.js
@@ -1,57 +1,63 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../../Assets/Logo2.png'
-import Footer from '../../Components/Footer/Footer'
-import './PhotographerSignup.css'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from '../../Assets/Logo2.png';
+import Footer from '../../Components/Footer/Footer';
+import axios from 'axios';
+import './PhotographerSignup.css';
 
-import user_icon from '../../Assets/person.png'
-import password_icon from '../../Assets/password.png'
+function PhotographerSignup() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+  const navigate = useNavigate();
 
-const PhotographerSignup = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:3002/api/photographer/register', { username, password, email, contactNumber })
+      .then(result => {
+        console.log(result);
+        navigate('/photographer/home');
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className='signup-background'>
       <div>
-          <nav className='navbar-choose'>
-                <div className='logo-head'>
-                <img src={logo} alt="" className='logo'/>
-                <div className='logo-text'>
-                    <h2>ShutterShare</h2>
-                    <p>Future Photography</p>
-                </div>
-                  
-                </div>
-                
-                
-                <ul>
-                    <li><Link to="/" className='action'>Home</Link></li>
-                    <Link to="/login" ><button className='basic-btn' >Login</button></Link>
-                    <Link to="/choose" ><button className='basic-btn'>Signup</button></Link>
-                </ul>
-
-                
-
-            </nav>
+        <nav className='navbar-choose'>
+          <div className='logo-head'>
+            <img src={logo} alt="logo" className='logo' />
+            <div className='logo-text'>
+              <h2>ShutterShare</h2>
+              <p>Future Photography</p>
+            </div>
+          </div>
+          <ul>
+            <li><Link to="/" className='action'>Home</Link></li>
+            <Link to="/login"><button className='basic-btn'>Login</button></Link>
+            <Link to="/choose"><button className='basic-btn'>Signup</button></Link>
+          </ul>
+        </nav>
       </div>
-          
       <div className='signup-section'>
         <div className='signup-info'>
-            <h1>Sign Up </h1>
-            <div className='line-choose-signup'></div>
-            <input type='text' placeholder='Enter Username'/>
-            <input type='password' placeholder='Enter Password' />
-            <input type='email' placeholder='Enter Email Address'/>
-            <input type='text' placeholder='Enter Contact Number' />
-            <Link to="/photographer/home"><button className='signup-btn'>Sign up</button></Link>
-            <p>Already have an account. <Link to="/login" className='link-login'>Login</Link></p>
+          <h1>Sign Up</h1>
+          <div className='line-choose-signup'></div>
+          <form onSubmit={handleSubmit}>
+            <input type='text' placeholder='Enter Username' onChange={(e) => setUsername(e.target.value)} />
+            <input type='password' placeholder='Enter Password' onChange={(e) => setPassword(e.target.value)} />
+            <input type='email' placeholder='Enter Email Address' onChange={(e) => setEmail(e.target.value)} />
+            <input type='text' placeholder='Enter Contact Number' onChange={(e) => setContactNumber(e.target.value)} />
+            <button type='submit' className='signup-btn'>Sign up</button>
+          </form>
+          <p>Already have an account? <Link to="/login" className='link-login'>Login</Link></p>
         </div>
-           
       </div>
-
-      <div>
-        <Footer/>
-      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default PhotographerSignup
+export default PhotographerSignup;
+
